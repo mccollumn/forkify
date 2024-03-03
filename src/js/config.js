546528e1926162adea42327fbbb7c6ce9cad8eb1
Nick McCollum <mccollumn@webtrends.com>
2024-03-03 15:36:44 -0800

    Add upload recipe feature

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '30d70b72-2316-4fed-a8de-f3943032f4df';
+export const MODAL_CLOSE_SEC = 2.5;
